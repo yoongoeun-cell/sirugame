befe42b36785ae70958c6f688790745fb2e3ee12
yoongoeun-cell <dbsrhdms0114@naver.com>
2025-07-03 16:37:08 +0900

    Fix Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/sirugame/", // 이 부분 추가
   plugins: [react()],
+  base: "/sirugame/",
 });
